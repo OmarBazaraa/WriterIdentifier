19b713a3f3aaa0c429bafce91f04a0b5a77e4e3e
OmarBazaraa <omar_bazaraa@hotmail.com>
2018-11-28 22:42:31 +0200

    Enhance line segmentation.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 import cv2 as cv
 import numpy as np
+import time
 
 from src.pre_processor import PreProcessor
 from src.segmentation.line_segmentor import LineSegmentor
 from src.feature_extractor import FeatureExtractor
 from src.utils.utils import *
+
+# Get start running time
+start_time = time.time()
 
 # Data set (i.e. lists of features and labels)
 features = []
@@ -34,9 +38,15 @@ for root, dirs, files in os.walk(data_path + "/"):
         gray_img, bin_img = PreProcessor.process(gray_img)
 
         # Line segment
-        # gray_lines, bin_lines = LineSegmentor.segment(gray_img, bin_img)
+        gray_lines, bin_lines = LineSegmentor.segment(gray_img, bin_img)
 
         # Extract features.
         features.append(FeatureExtractor.extract_features(gray_img, bin_img))
 
         # TODO Extract label (i.e. writer id).
+
+# Get finish running time
+finish_time = time.time()
+
+# Print elapsed execution time
+print("This took %.2f seconds" % (finish_time - start_time))
--- src/segmentation/line_segmentor.py
+++ src/segmentation/line_segmentor.py
@@ -10,57 +10,114 @@ class LineSegmentor:
     @staticmethod
     def segment(gray_img, bin_img):
         # Get image dimensions.
-        h, w = gray_img.shape
+        height, width = gray_img.shape
 
         # Get horizontal histogram.
         hor_hist = np.sum(bin_img, axis=1) / 255
 
-        mean = hor_hist.mean()
-        median = np.median(hor_hist)
-        half = np.max(hor_hist) / 2
+        # Get line density threshold.
+        threshold = int(np.max(hor_hist) / 2)
 
-        # Threshold values.
-        threshold_val = 10  # Maximum number of black pixels in a row to consider it blank row
-        threshold_cnt = 10  # Minimum number of contiguous blank rows to consider it blank line
-
-        # List of blank rows
-        blank_rows = [0]
-
-        # Detect blank contiguous rows.
+        # Detect peak rows.
+        peaks = []
         i = 0
-        while i < h:
+        while i < len(hor_hist):
             # Check if line is not empty
-            if hor_hist[i] > threshold_val:
+            if hor_hist[i] < threshold:
                 i += 1
                 continue
 
-            # Get contiguous blank (white) rows.
-            j = min_idx = i
-            while j < h and hor_hist[j] <= threshold_val:
+            # Get peak row.
+            j = max_idx = i
+            while j < len(hor_hist) and (
+                    hor_hist[j] >= threshold or
+                    hor_hist[j - 1] >= threshold or
+                    hor_hist[j + 1] >= threshold
+            ):
+                if hor_hist[j] > hor_hist[max_idx]:
+                    max_idx = j
+                j += 1
+            i = j
+
+            # Add peak row index to the list
+            peaks.append(max_idx)
+
+        # Calculate average distance between lines.
+        avg_dis = 0
+        i = 1
+        while i < len(peaks):
+            avg_dis += peaks[i] - peaks[i - 1]
+            i += 1
+        avg_dis /= len(peaks)
+
+        # Detect valleys between consecutive peaks.
+        valleys = [0]
+        i = 1
+        while i < len(peaks):
+            u = peaks[i - 1]
+            d = peaks[i]
+            i += 1
+            j = min_idx = u
+            while j < d:
                 if hor_hist[j] < hor_hist[min_idx]:
                     min_idx = j
                 j += 1
+            valleys.append(min_idx)
+        valleys.append(len(hor_hist) - 1)
 
-            # Add blank line if its height is not less than the threshold
-            if j - i >= threshold_cnt:
-                blank_rows.append(min_idx)
+        # Detect line boundaries.
+        lines = []
+        i = 1
+        while i < len(valleys):
+            u = valleys[i - 1]
+            d = valleys[i]
+            l = 0
+            r = width - 1
 
-            i = j
+            while u < d and hor_hist[u] == 0:
+                u += 1
+            while d > u and hor_hist[d] == 0:
+                d -= 1
 
-        blank_rows.append(h - 1)
+            ver_hist = np.sum(bin_img[u:d + 1, :], axis=0) / 255
 
-        print(blank_rows)
+            while l < r and ver_hist[l] == 0:
+                l += 1
+            while r > l and ver_hist[r] == 0:
+                r -= 1
 
-        for r in blank_rows:
-            cv.line(bin_img, (0, r), (w, r), 255, 2)
+            lines.append((l, u, r, d))
 
-        display_image('Img', bin_img, False)
+            i += 1
 
+        #
+        # Illustrate line segmentation.
+        #
+
+        # Draw bounding box around lines.
+        img = cv.cvtColor(gray_img, cv.COLOR_GRAY2BGR)
+
+        for l, u, r, d in lines:
+            cv.rectangle(img, (l, u), (r, d), (0, 0, 255), 2)
+
+        display_image('Binary Paragraph', img, False)
+
+        # Draw histogram
         plt.figure()
-        plt.plot(list(range(h)), hor_hist)
-        plt.plot([0, h], [mean, mean], 'r')
-        plt.plot([0, h], [median, median], 'c')
-        plt.plot([0, h], [half, half], 'g')
+        plt.xlabel('Row index')
+        plt.ylabel('Number of black pixels')
+        plt.plot(list(range(len(hor_hist))), hor_hist)
+        plt.plot([0, len(hor_hist)], [threshold, threshold], 'g--')
+
+        # Draw peaks.
+        for r in peaks:
+            plt.plot(r, hor_hist[r], 'ro')
+            plt.plot([r, r + avg_dis], [hor_hist[r], hor_hist[r]], 'r')
+
+        # Draw valleys.
+        for r in valleys:
+            plt.plot(r, hor_hist[r], 'bs')
+
         plt.show()
 
         return None, None
